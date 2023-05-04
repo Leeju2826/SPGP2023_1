724efad01f09149326d524487caf12c05fb74442
Leeju2826 <44462330+Leeju2826@users.noreply.github.com>
2023-05-04 21:30:22 +0900

    FrameWork
    프레임워크

---
+++ app/src/main/java/com/example/spgp_termproject_2017180031/framework/IGameObject.java
@@ -0,0 +1,8 @@
+package com.example.spgp_termproject_2017180031.framework;
+
+import android.graphics.Canvas;
+
+public interface IGameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
