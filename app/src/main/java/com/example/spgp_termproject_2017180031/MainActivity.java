94896d86a4027a9c7430e5f40ce3aed9b1b3b05c
Leeju2826 <44462330+Leeju2826@users.noreply.github.com>
2023-05-04 21:09:11 +0900

    Organize_sketeton_1
    패키지 정리 및 리소스 파일 임시 이동

--- app/src/main/java/com/example/spgp_termproject_2017180031/MainActivity.java
+++ app/src/main/java/com/example/spgp_termproject_2017180031/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.spgp_termproject_2017180031;
+package com.example.spgp_termproject_2017180031.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.spgp_termproject_2017180031.R;
 
 public class MainActivity extends AppCompatActivity {
 
